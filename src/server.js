e1e7d8d427cc6f40ed053d5a256485e79ca1c94d
5791-IM <142788119+5791-IM@users.noreply.github.com>
2024-07-24 16:29:17 +0300

    +-

--- src/server.js
+++ src/server.js
@@ -2,8 +2,7 @@ import { env } from './utils/env.js';
 import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
-
-import { getAllContacts, getContactById } from './services/conacts.js';
+import { getAllContacts, getContactById } from './services/contacts.js';
 
 const PORT = Number(env('PORT', '3000'));
 
