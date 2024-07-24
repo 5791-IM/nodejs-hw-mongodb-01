6df055a004b0bcab2facf379cf8fe7c9127289fb
5791-IM <142788119+5791-IM@users.noreply.github.com>
2024-07-24 20:02:17 +0300

    Update

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -6,11 +6,11 @@ export const initMongoConnection = async () => {
   const user = env('MONGODB_USER');
   const password = env('MONGODB_PASSWORD');
   const url = env('MONGODB_URL');
-  const db = env('MONGODB_DB');
+  const dbName = env('MONGODB_DB');
 
   try {
     await mongoose.connect(
-      `mongodb+srv://${user}:${password}@${url}/${db}?retryWrites=true&w=majority&appName=Cluster0`,
+      `mongodb+srv://${user}:${password}@${url}/${dbName}?retryWrites=true&w=majority&appName=Cluster0`,
     );
     console.log('Connection successfully');
   } catch (error) {
