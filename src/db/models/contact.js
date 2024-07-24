1f503db88c03ae761497262d59785d02c94fa4bd
5791-IM <142788119+5791-IM@users.noreply.github.com>
2024-07-24 13:37:56 +0300

    create new project

---
+++ src/db/models/contact.js
@@ -0,0 +1,34 @@
+import { model, Schema } from 'mongoose';
+// import mongoose from 'mongoose';
+
+const contactSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: Number,
+      required: true,
+    },
+    email: {
+      type: String,
+    },
+    isFavourite: {
+      type: Boolean,
+      default: false,
+    },
+    contactType: {
+      type: String,
+      enum: ['work', 'home', 'personal'],
+      required: true,
+      default: 'personal',
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  },
+);
+
+export const ContactsCollection = model('contacts', contactSchema);
