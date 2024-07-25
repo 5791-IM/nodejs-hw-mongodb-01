0f9e13fcaa3f2bdc8a60c720deaed843e42aa093
5791-IM <142788119+5791-IM@users.noreply.github.com>
2024-07-26 00:41:50 +0300

    - +

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -95,33 +95,3 @@ export const patchContactController = async (req, res, next) => {
     data: result.contact,
   });
 };
-
-// import { getAllContacts, getContactById } from '../services/contacts';
-
-// export const getContactsController = async (req, res) => {
-//   const contacts = await getAllContacts();
-
-//   res.json({
-//     status: 200,
-//     message: 'Successfully found contacts',
-//     data: contacts,
-//   });
-// };
-
-// export const getContactByIdController = async (req, res) => {
-//   const { contactId } = req.params;
-//   const contact = await getContactById(contactId);
-
-//   if (!contact) {
-//     res.status(404).json({
-//       message: 'Contact not found',
-//     });
-//     return;
-//   }
-
-//   res.json({
-//     status: 200,
-//     message: `Successfully found contact with id ${contactId}!`,
-//     data: contact,
-//   });
-// };
