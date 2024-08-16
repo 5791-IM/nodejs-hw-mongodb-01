0a0a9964b4b9322fcec04a12c94249c64c3a2241
5791-IM <142788119+5791-IM@users.noreply.github.com>
2024-08-16 20:28:49 +0300

    +-

--- src/db/models/session.js
+++ src/db/models/session.js
@@ -2,13 +2,30 @@ import { model, Schema } from 'mongoose';
 
 const sessionsSchema = new Schema(
   {
-    userId: { type: Schema.Types.ObjectId, ref: 'user' },
-    accessToken: { type: String, required: true },
-    refreshToken: { type: String, required: true },
-    accessTokenValidUntil: { type: Date, required: true },
-    refreshTokenValidUntil: { type: Date, required: true },
+    userId: {
+      type: Schema.Types.ObjectId,
+      // ref: 'user'
+      required: true,
+      unique: true,
+    },
+    accessToken: {
+      type: String,
+      required: true,
+    },
+    refreshToken: {
+      type: String,
+      required: true,
+    },
+    accessTokenValidUntil: {
+      type: Date,
+      required: true,
+    },
+    refreshTokenValidUntil: {
+      type: Date,
+      required: true,
+    },
   },
   { timestamps: true, versionKey: false },
 );
 
-export const SessionsCollection = model('sessions', sessionsSchema);
+export const SessionsCollection = model('session', sessionsSchema);
--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -13,8 +13,6 @@ import { createContactSchema } from '../validation/contacts.js';
 import { updateContactSchema } from '../validation/contacts.js';
 import { isValidId } from '../middlewares/isValidId.js';
 import { authenticate } from '../middlewares/authenticate.js';
-import { checkRoles } from '../middlewares/checkRoles.js';
-import { ROLES } from '../constants/index.js';
 
 const router = Router();
 
@@ -22,33 +20,21 @@ router.use(authenticate);
 
 router.get('/', ctrlWrapper(getContactsController));
 
-router.get(
-  '/:contactId',
-  checkRoles(ROLES.TEACHER, ROLES.PARENT),
-  isValidId,
-  ctrlWrapper(getContactByIdController),
-);
+router.get('/:contactId', isValidId, ctrlWrapper(getContactByIdController));
 
 router.post(
-  '/',
-  checkRoles(ROLES.TEACHER),
+  '/register',
   validateBody(createContactSchema),
   ctrlWrapper(createContactController),
 );
 
+router.delete('/:contactId', isValidId, ctrlWrapper(deleteContactController));
+
 router.patch(
   '/:contactId',
-  checkRoles(ROLES.TEACHER, ROLES.PARENT),
   isValidId,
   validateBody(updateContactSchema),
   ctrlWrapper(patchContactController),
-);
-
-router.delete(
-  '/:contactId',
-  checkRoles(ROLES.TEACHER),
-  isValidId,
-  ctrlWrapper(deleteContactController),
 );
 
 export default router;
